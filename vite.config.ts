ba6a3b2d13b341d3c50ada212d353ff2320fbf53
Rosh <roshanibankar11@gmail.com>
2026-02-19 17:43:38 +0530

    lol

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/iPortfolio/',   // 👈 MUST match repo name
+  base: '/iPortfolio/',   // MUST match repo name
 })
